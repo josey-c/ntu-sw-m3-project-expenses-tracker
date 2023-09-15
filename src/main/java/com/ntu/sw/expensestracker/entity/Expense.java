ba331e20a009de3a3924da3dc234a88ba02788e7
nurulhafizahharon <nurulhafizahharon@outlook.com>
2023-09-15 15:58:43 +0800

    Link Expense table to Category table

--- src/main/java/com/ntu/sw/expensestracker/entity/Expense.java
+++ src/main/java/com/ntu/sw/expensestracker/entity/Expense.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -22,11 +24,18 @@ public class Expense {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
     @Column(name = "date")
     private LocalDate expenseDate;
-    // @Column(name = "expense")
-    // private String expense;
+
     @Column(name = "description")
     private String description;
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @OneToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
 
 }
